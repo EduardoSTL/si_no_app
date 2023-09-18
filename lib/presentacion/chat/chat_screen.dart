06e3b21086a1e4911d2619fbeeaf5a3f85c0077a
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-18 09:32:20 -0600

    added: nav bar for chat

--- lib/presentacion/chat/chat_screen.dart
+++ lib/presentacion/chat/chat_screen.dart
@@ -5,6 +5,7 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //* Scaffold > Pantalla > AppBar
     return Scaffold(
       appBar: AppBar(
         leading: const Padding(
@@ -17,6 +18,29 @@ class ChatScreen extends StatelessWidget {
           ),
           title: const Text('Eduardo'),
           centerTitle: false,
+      ),
+      //* Barra de chat (nuevo widget):
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+            ),
+            const Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Escribe un mensaje',
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.send),
+            ),
+          ],
+        ),
       ),
     );
   }
