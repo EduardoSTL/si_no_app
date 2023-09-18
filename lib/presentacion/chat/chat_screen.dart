f77d269dbbc5d82da5aabff9c1f1c699d032d799
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-18 08:57:34 -0600

    updt: chat screen

---
+++ lib/presentacion/chat/chat_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(3.0), 
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png'
+            ),
+          ),
+          ),
+      ),
+    );
+  }
+}
