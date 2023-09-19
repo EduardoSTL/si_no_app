44cf2774cf93595e957513b2dd1ce05f6d486179
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-19 10:21:00 -0600

    updt: gif view

--- lib/presentacion/widgets/chat/otro_message_burbuja.dart
+++ lib/presentacion/widgets/chat/otro_message_burbuja.dart
@@ -22,21 +22,35 @@ class OtroMyMessageBubble extends StatelessWidget {
             ),
         ),
         const SizedBox(
+          height: 10),
+        _ImageBubble(),
+        const SizedBox(
           height: 5),
-        const _ImageBubble(),
       ],
     );
   }
 }
 
 class _ImageBubble extends StatelessWidget {
-  const _ImageBubble({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
-      child: Image.network('https://yesno.wtf/assets/no/6-4bf0a784c173f70a0cab96efd9ff80c9.gif'),
+      child: Image.network('https://yesno.wtf/assets/no/6-4bf0a784c173f70a0cab96efd9ff80c9.gif',
+      width: size.width * 0.7,
+      height: 150,
+      fit: BoxFit.cover,
+      loadingBuilder: (context, child, loadingProgress){
+        if(loadingProgress == null) return child;
+        return Container(
+          width: size.width * 0.7,
+          height: 150,
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          child: const Text('estoy enviando una imagen'),
+          );
+      }),
     );
   }
 }
