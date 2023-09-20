0237d8023807253b2fd73a1c72ab998791f2541b
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-20 11:55:17 -0600

    added: answer gift algoritm

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -1,8 +1,15 @@
+import 'package:dio/dio.dart';
 import 'package:si_no_app/domain/entities/message.dart';
 
 class GetYesNoAnswer{
-  Future<Message> getAnswer(){
-    
-    return;
+  final _dio = Dio();
+
+  Future<Message> getAnswer() async {
+    final response = await _dio.get('https://yesno.wtf/api');
+    //implementacion errores HTTP(400)e
+    return Message(text: response.data['answer'],
+    fromWho: FromWho.other,
+    imageUrl: response.data['image']
+    );
   }
 }
--- lib/presentacion/provider/chat_provider.dart
+++ lib/presentacion/provider/chat_provider.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:si_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:si_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier{
-  //instanciacion de clase directamente usando el constrct. de la clase
+  //* variable para traer protocolo http:
+  final getYesNoAnswer = GetYesNoAnswer();
+
+  //* instanciacion de clase directamente usando el constrct. de la clase:
   final ScrollController chatScrollController = ScrollController();
 
   //* Lista de mensajes predefinidos
   List<Message> messageList = [
     Message(text: 'Que ondas', fromWho: FromWho.mine),
-    Message(text: 'Ya regresaste?', fromWho: FromWho.other),
+    Message(text: 'ensalada', fromWho: FromWho.mine),
 
   ];
 
@@ -17,6 +21,9 @@ class ChatProvider extends ChangeNotifier{
     if(text.isEmpty) return;
     final newMessage = Message(text: text, fromWho: FromWho.mine);
     messageList.add(newMessage);
+    if(text.endsWith('?')){
+      await otherReply();
+    }
     notifyListeners();
     moveScrollToBottom();
   }
@@ -28,5 +35,12 @@ class ChatProvider extends ChangeNotifier{
       chatScrollController.position.maxScrollExtent,
       duration: const Duration(milliseconds: 300),
       curve: Curves.easeOut);
+  }
+
+  Future<void> otherReply() async{
+    final otherMessage = await getYesNoAnswer.getAnswer(); 
+    messageList.add(otherMessage);
+    notifyListeners();
+    moveScrollToBottom();
   }
 }
