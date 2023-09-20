c3fff11001d0f63568903646b03ac106ab46c119
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-20 12:12:18 -0600

    updt: yes no answ

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -1,15 +1,18 @@
 import 'package:dio/dio.dart';
 import 'package:si_no_app/domain/entities/message.dart';
+import 'package:si_no_app/infraestructure/models/yes_no_model.dart';
 
 class GetYesNoAnswer{
   final _dio = Dio();
 
   Future<Message> getAnswer() async {
     final response = await _dio.get('https://yesno.wtf/api');
+    final yesNoModel = YesNoModel.fromJsonMap(response.data);
     //implementacion errores HTTP(400)e
-    return Message(text: response.data['answer'],
+    return Message(
+      text: yesNoModel.answer,
     fromWho: FromWho.other,
-    imageUrl: response.data['image']
+    imageUrl: yesNoModel.image
     );
   }
 }
