a24a3f96f0bd2814c0f22fa315dd9e526ef48e25
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-21 08:29:18 -0600

    updt: yes no answer

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -7,12 +7,8 @@ class GetYesNoAnswer{
 
   Future<Message> getAnswer() async {
     final response = await _dio.get('https://yesno.wtf/api');
-    final yesNoModel = YesNoModel.fromJsonMap(response.data);
+    final yesNoModel = YesNoModel.fromJson(response.data);
     //implementacion errores HTTP(400)e
-    return Message(
-      text: yesNoModel.answer,
-    fromWho: FromWho.other,
-    imageUrl: yesNoModel.image
-    );
+    return yesNoModel.toMessageEntity();
   }
 }
